968e2db2a7ff8b4916b11c29bdfc4a298f33999c
nhrnhr0 <nhrnhr0@gmail.com>
2023-03-01 13:40:42 +0200

    t

--- server/server/settings/production.py
+++ server/server/settings/production.py
@@ -4,9 +4,9 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['gold-tv-server.boost-pop.com', 'gold-tv.boost-pop.com']
-CSRF_TRUSTED_ORIGINS = ['https://gold-tv-server.boost-pop.com', 'http://gold-tv-server.boost-pop.com',
-                        'https://gold-tv.boost-pop.com', 'http://gold-tv.boost-pop.com']
+ALLOWED_HOSTS = ['gold-tv-server2.boost-pop.com', 'gold-tv.boost-pop.com']
+CSRF_TRUSTED_ORIGINS = ['https://gold-tv-server2.boost-pop.com', 'http://gold-tv-server2.boost-pop.com',
+                        'https://gold-tv2.boost-pop.com', 'http://gold-tv2.boost-pop.com']
 
 DATABASES = {
     "default": {
