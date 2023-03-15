a8e04717f2e747a95237a1ff7d7ab5873bf48e59
Ubuntu <ubuntu@ip-172-31-29-163.us-east-2.compute.internal>
2023-03-15 09:37:59 +0000

    fix deploy script to multiple daphne workers

--- deplay.sh
+++ deplay.sh
@@ -3,7 +3,6 @@ source env/bin/activate
 pip install -r requirements.txt
 python server/manage.py migrate 
 env/bin/python server/manage.py collectstatic --noinput
-sudo systemctl daemon-reload
-sudo systemctl restart daphne
+sudo supervisorctl restart asgi
 sudo service nginx restart
 echo 'done'
