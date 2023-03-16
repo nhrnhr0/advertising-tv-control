7f178f853e2b593286473f07356831def1615029
nhrnhr0 <nhrnhr0@gmail.com>
2023-03-16 09:08:22 +0200

    adding celery setup script

--- init_celery.sh
+++ init_celery.sh
@@ -1,6 +1,7 @@
 
 sudo cp ./setup/adv_control_celery.conf /etc/supervisor/conf.d/adv_control_celery.conf
 sudo cp ./setup/adv_control_celerybeat.conf /etc/supervisor/conf.d/adv_control_celerybeat.conf
+sudo mkdir -p /var/log/celery
 sudo supervisorctl reread
 sudo supervisorctl update
 sudo supervisorctl restart adv_control_celery
