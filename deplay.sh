4f679397e912a8a5239fbd8c5734ac0a3a7d757b
Ubuntu <ubuntu@ip-172-31-29-163.us-east-2.compute.internal>
2023-03-18 20:20:11 +0000

    fix deploy

--- deplay.sh
+++ deplay.sh
@@ -3,6 +3,6 @@ source env/bin/activate
 pip install -r requirements.txt
 python server/manage.py migrate 
 env/bin/python server/manage.py collectstatic --noinput
-sudo supervisorctl restart asgi
+sudo supervisorctl restart all
 sudo service nginx restart
 echo 'done'
