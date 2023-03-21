3b2ccbec8fc0e4a0b1ab1ba33eedfa7a48c2bf04
nhrnhr0 <nhrnhr0@gmail.com>
2023-03-21 16:50:17 +0200

    fix setup script

--- run.sh
+++ run.sh
@@ -7,14 +7,10 @@ sudo apt install redis
 sudo systemctl restart redis-server
 
 
-sudo cp setup/daphne.socket /etc/systemd/system/daphne.socket
-sudo cp setup/daphne.service /etc/systemd/system/daphne.service
-
-sudo systemctl start daphne.socket
-sudo systemctl enable daphne.socket
-
-sudo systemctl daemon-reload
-sudo systemctl restart daphne
+sudo cp setup/daphne_workers.conf /etc/supervisor/conf.d/daphne_workers.conf
+sudo supervisorctl reread
+sudo supervisorctl update
+sudo supervisorctl restart daphne_workers
 
 sudo cp setup/adv_tv_nginx /etc/nginx/sites-available/kehilaton_nginx.conf
 sudo ln -s /etc/nginx/sites-available/kehilaton_nginx.conf /etc/nginx/sites-enabled
